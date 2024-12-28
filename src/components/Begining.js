30aa5045194f90cfeeabb588f93f3accea89246d
Mahmut Tunahan Aktaş <128902817+Fastbackk@users.noreply.github.com>
2024-12-28 10:11:24 +0300

    commit after school

--- src/components/Begining.js
+++ src/components/Begining.js
@@ -5,10 +5,12 @@ export default function Begining() {
         <div className='Begin'>
             <div className='left-div'>
                 <div style={{ width: '80%', marginTop: '30%' }}>
-                    <p>
-                        <h1><strong>Merhaba! </strong><br></br></h1>
+                    <p className="begining-p">
+                        <h1><strong>Merhaba! </strong></h1>
 
-                        Ben Tunahan Aktaş. Öğrenmeyi tutkuyla seven bir öğrenci ve geleceğin yazılım dünyasında iz bırakmayı hedefleyen bir full-stack developer. Boş vakitlerimde kitapların büyülü dünyasında kaybolur, yeni hikayelere yelken açarım. Her satır kodda, her sayfada yeni bir serüven bulurum.
+                        Ben Tunahan. Öğrenmeyi tutkuyla seven bir öğrenci ve geleceğin yazılım dünyasında iz bırakmayı hedefleyen bir full-stack developer.
+                        Boş vakitlerimde kitapların büyülü dünyasında kaybolur, yeni hikayelere yelken açarım.
+                        Her satır kodda, her sayfada yeni bir serüven bulurum.
 
                     </p>
                     <div className='contact-begining'>
@@ -17,10 +19,12 @@ export default function Begining() {
                 </div>
             </div>
             <div className='middle-div'>
-                <div style={{ background: 'black', height: '40vh', width: '40vh', borderRadius: '100%' }}></div>
+                <img src='../photos/photo3.jpg' style={{ border:'3px solid #0e3865',background: 'black', height: '400px', width: '400px',borderRadius:'400px'  }}></img>
 
             </div>
             <div className='right-div'>
+                <h1 className='name-p'>Mahmut <br/>Tunahan <br/>Aktaş</h1>
+                
 
             </div>
         </div>
